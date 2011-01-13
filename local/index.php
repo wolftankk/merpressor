0990a0c13417f666747c72fd56fbe4f053db64a9
xushengs <xushengs@b363e5f4-2d12-9f21-29c9-bac9edaae94c>
2011-01-13 06:11:40 +0000

    修改URL重写规则，把版本号提到文件名之前，以便同步更新背景图版本号。

--- local/index.php
+++ local/index.php
@@ -11,7 +11,7 @@ body{text-align:center;padding:32px 16px;margin:0;}
 	<h2>Welcome to Merpressor!</h2>
 	<hr />
 	<p>
-		Merpressor 1.0.0, 05 Aug 2010
+		Merpressor 1.0.3, 12 Jan. 2011
 	</p>
 </body>
 </html>
